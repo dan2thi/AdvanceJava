70176a59180b88496fbcbeebd9c5904cbbebdaee
dan2thi <dan2thi@gmail.com>
2022-07-28 23:37:31 -0400

    Moved existing source for functions to a new module named Functions

--- Functions/src/functions/tri/TriFunction.java
+++ Functions/src/functions/tri/TriFunction.java
@@ -3,6 +3,7 @@ package functions.tri;
 /*
     This is a custom interface (i.e not provided by java) to support functions with 3 arguments with a return type
  */
+@FunctionalInterface
 public interface TriFunction<T, U, V, R> {
 
     R apply(T t, U u, V v);
